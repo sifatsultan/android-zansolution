5e4a22b58e0eec3ccee174d4b929053cf6c1034d
Sifat Sultan <sifatsultan@gmail.com>
2016-01-28 08:40:19 +0800

    onItemClickListener actually words

--- app/src/main/java/sifat/zansolution/PortfolioFragment.java
+++ app/src/main/java/sifat/zansolution/PortfolioFragment.java
@@ -1,24 +1,74 @@
 package sifat.zansolution;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Sifat on 1/2/2016.
  */
 public class PortfolioFragment extends Fragment {
+
     ListView lv;
+    int[] banners = new int[]{R.drawable.banner1, R.drawable.banner2, R.drawable.banner3, R.drawable.banner4, R.drawable.banner5, R.drawable.banner6, R.drawable.banner7, R.drawable.banner8, R.drawable.banner9, R.drawable.banner10,R.drawable.banner11, R.drawable.banner12};
+    String para = "Azole glauberite untwitching palpable bear portmanteau ungentlemanlike theobromine desmona diapedetic outquoting arr undergardener raja. Methadone undemocratic thyatira materialiser dauber herzl satiric branchiopneustic retroflexion exigency bornholm bade ahmadiya  powdiving. Euphuistically homestead beatty tungstate uncharacteristic semichaotically oniomaniac sharpie thyroiditis defunctness expertly epiclesis undocumented. Fayalite unabjured blender undertrader yesterweek awry manganin jamboree criminator laemmle geothermal disrepair lymphangiomas mottler. Swordmanship galton superillustration nonextinction fagging crimean inclementness gilder unhuman defective grewsomely generable jaundicing institutionary";
+    int[] logo = banners;
+    ArrayList<Uri> bannerUri = getUris(banners);
+    ArrayList<Uri> logoUri = getUris(logo);
+    ArrayList<String> title = getSomeWords(banners.length, para);
+    ArrayList<String> tags = getHashed(title);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_portfolio,container, false);
         lv = (ListView)rootView.findViewById(R.id.listView);
-        lv.setAdapter(new TheAdapter(getActivity()));
-        return rootView;
+        lv.setAdapter(new TheAdapter(getActivity(),bannerUri, logoUri,title,tags));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String msg = "banners: "+bannerUri.get(position)+" logo: "+logoUri.get(position)+" title :"+title.get(position)+" tags "+tags.get(position);
+                Toast.makeText(getActivity(),msg,Toast.LENGTH_SHORT).show();
 
+            }
+        });
+        return rootView;
     }
+
+    public ArrayList<String> getHashed(ArrayList<String> title) {
+        ArrayList<String> tags = new ArrayList<String>();
+
+        for (String item : title) {
+            String tag = "#" + item;
+            tags.add(tag);
+        }
+        return tags;
+    }
+
+    public ArrayList<Uri> getUris(int[] res) {
+        ArrayList<Uri> uris = new ArrayList<Uri>();
+        for (int item : res) {
+            String path = "res:/" + String.valueOf(item);
+            uris.add(Uri.parse(path));
+        }
+        return uris;
+    }
+
+    public ArrayList<String> getSomeWords(int num, String text) {
+        String[] words = text.split("\\s+");
+        ArrayList<String> someWords = new ArrayList<>();
+
+        for (int j = 0; j < num; j++) {
+            someWords.add(words[j]);
+        }
+        return someWords;
+    }
+
 }
--- app/src/main/java/sifat/zansolution/TheAdapter.java
+++ app/src/main/java/sifat/zansolution/TheAdapter.java
@@ -18,24 +18,25 @@ import java.util.ArrayList;
  */
 public class TheAdapter extends BaseAdapter {
 
-    int[] banners = new int[]{R.drawable.banner1, R.drawable.banner2, R.drawable.banner3, R.drawable.banner4, R.drawable.banner5, R.drawable.banner6, R.drawable.banner7, R.drawable.banner8, R.drawable.banner9, R.drawable.banner10,R.drawable.banner11, R.drawable.banner12};
-    int[] logo = banners;
-    ArrayList<Uri> bannerUri = getUris(banners);
-    ArrayList<Uri> logoUri = getUris(logo);
-    ArrayList<String> title = getSomeWords(banners.length);
-    ArrayList<String> tags = getHashed(title);
     Context mContext;
     private LayoutInflater mLayoutInflater;
+    ArrayList<Uri> bannerUri, logoUri;
+    ArrayList<String> title,tags;
 
-    public TheAdapter(Context context) {
+
+    public TheAdapter(final Context context, ArrayList<Uri> banners, ArrayList<Uri> logo, ArrayList<String> title, ArrayList<String> tags) {
         this.mContext = context;
         mLayoutInflater = (LayoutInflater) mContext
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.bannerUri = banners;
+        this.logoUri = logo;
+        this.title = title;
+        this.tags = tags;
     }
 
     @Override
     public int getCount() {
-        return banners.length;
+        return bannerUri.size();
     }
 
     @Override
@@ -47,8 +48,6 @@ public class TheAdapter extends BaseAdapter {
     public long getItemId(int position) {
         return 0;
     }
-
-
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -63,53 +62,16 @@ public class TheAdapter extends BaseAdapter {
 
         SimpleDraweeView logoView = (SimpleDraweeView) itemView.findViewById(R.id.logo);
         SimpleDraweeView bannerView = (SimpleDraweeView) itemView.findViewById(R.id.banner);
-
-//        ImageView logoView = (ImageView)itemView.findViewById(R.id.logo);
-//        ImageView bannerView = (ImageView)itemView.findViewById(R.id.banner);
         TextView titleView = (TextView) itemView.findViewById(R.id.title);
         TextView tagsView = (TextView) itemView.findViewById(R.id.tags);
 
 //        String path = "res:/" + R.drawable.default_app; // Only one slash after res:
-
         logoView.setImageURI(logoUri.get(position));
         bannerView.setImageURI(bannerUri.get(position));
-        titleView.setText(title.get(position));
+        titleView.setText(title.get(position).toUpperCase());
         tagsView.setText(tags.get(position));
 
         return itemView;
-    }
-
-
-    public ArrayList<String> getHashed(ArrayList<String> title) {
-        ArrayList<String> tags = new ArrayList<String>();
-
-        for (String item : title) {
-            String tag = "#" + item;
-            tags.add(tag);
-        }
-
-        return tags;
-    }
-
-    public ArrayList<Uri> getUris(int[] res) {
-        ArrayList<Uri> uris = new ArrayList<Uri>();
-        for (int item : res) {
-            String path = "res:/" + String.valueOf(item);
-            uris.add(Uri.parse(path));
-        }
-        return uris;
-    }
-
-
-    public ArrayList<String> getSomeWords(int num) {
-        String para = "Azole glauberite untwitching palpable bear portmanteau ungentlemanlike theobromine desmona diapedetic outquoting arr undergardener raja. Methadone undemocratic thyatira materialiser dauber herzl satiric branchiopneustic retroflexion exigency bornholm bade ahmadiya powdiving. Euphuistically homestead beatty tungstate uncharacteristic semichaotically oniomaniac sharpie thyroiditis suppï¿¥ï¾½ defunctness expertly epiclesis undocumented. Fayalite unabjured blender undertrader yesterweek awry manganin jamboree criminator laemmle geothermal disrepair lymphangiomas mottler. Swordmanship galton superillustration nonextinction fagging crimean inclementness gilder unhuman defective grewsomely generable jaundicing institutionary";
-        String[] words = para.split("\\s+");
-        ArrayList<String> someWords = new ArrayList<>();
-
-        for (int j = 0; j < num; j++) {
-            someWords.add(words[j]);
-        }
-        return someWords;
     }
 
 }
